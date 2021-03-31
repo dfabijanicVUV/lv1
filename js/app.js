3617a1161fd897cc39ea37a943b1e43ba1a251b3
Dora Fabijanic <dora.fabijanic@vsmti.hr>
2021-03-31 10:04:54 +0200

    search commit

--- js/app.js
+++ js/app.js
@@ -25,5 +25,34 @@ function Delete(r) {
 
 function Color(r) {
 	var i = r.parentNode.parentNode;
-	i.setAttribute('style', 'background: yellow');
+	if(r.checked)
+	{
+		i.setAttribute('style', 'background: yellow');
+	}
+	else
+	{
+		i.setAttribute('style', 'background: white');
+	}
 }
+
+function Search() {
+	// Declare variables
+	var input, filter, table, tr, td, i, txtValue;
+	input = document.getElementById("myInput");
+	filter = input.value.toUpperCase();
+	table = document.getElementById("table");
+	tr = table.getElementsByTagName("tr");
+  
+	// Loop through all table rows, and hide those who don't match the search query
+	for (i = 0; i < tr.length; i++) {
+	  td = tr[i].getElementsByTagName("td")[0];
+	  if (td) {
+		txtValue = td.textContent || td.innerText;
+		if (txtValue.toUpperCase().indexOf(filter) > -1) {
+		  tr[i].style.display = "";
+		} else {
+		  tr[i].style.display = "none";
+		}
+	  }
+	}
+  }
