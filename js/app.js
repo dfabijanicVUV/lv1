2add2bbceefa47df85edc27b7a6c948cf1f34f5b
Dora Fabijanic <dora.fabijanic@vsmti.hr>
2021-03-31 09:53:49 +0200

    treci commit

--- js/app.js
+++ js/app.js
@@ -9,15 +9,21 @@ function Submit() {
     var cel2 = row.insertCell(1);
     var cel3 = row.insertCell(2);
 	var cel4 = row.insertCell(3);
+	var cel5 = row.insertCell(4);
 
 	cel1.innerHTML = nazivPredmeta;
 	cel2.innerHTML = godina;
 	cel3.innerHTML = ects;
 	cel4.innerHTML = '<button onclick="Delete(this)" style="background: url(images/trash-alt-regular.svg)">s</button>' ;
-	 
+	cel5.innerHTML = '<input onclick="Color(this)" type="checkbox" id="checkDone" name="checkDone">' 
 }	
 
 function Delete(r) {
 	var i = r.parentNode.parentNode.rowIndex;
     document.getElementById("table").deleteRow(i);
+}
+
+function Color(r) {
+	var i = r.parentNode.parentNode;
+	i.setAttribute('style', 'background: yellow');
 }
